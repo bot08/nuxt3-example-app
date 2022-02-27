5ff376e0549bd1823447f729902433305fe8eaaa
Oleg <scherbaoleg4@gmail.com>
2022-02-27 22:30:02 +0200

    init

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
+
+
+<script>
+import "assets/css/style.css"
+
+// TODO -> font to assets
+// TODO -> import to nuxt.config
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-2xl text-gray-700 my-2">Hello world</h1>
+</template>
