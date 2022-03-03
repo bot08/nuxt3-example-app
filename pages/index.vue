303a904e729019c17dd6948244046206ac6acbf9
Oleg <scherbaoleg4@gmail.com>
2022-03-03 14:59:15 +0200

    add heroicons

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <template>
     <div>
         <h1 class="text-2xl font-bold text-gray-700 dark:text-gray-200 my-2">
-            Hello world <br>
+            Hello world <StarIcon class="h-5 w-5 inline"/><br>
             Пример 
         </h1>
     </div>
 </template>
+
+
+<script>
+import { StarIcon } from '@heroicons/vue/solid'
+
+export default {
+  components: {
+    StarIcon
+  }
+}
+</script>
