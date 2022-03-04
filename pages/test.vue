3bfaa0042df7ea3cc9fc208ee33140d64fe6614b
Oleg <scherbaoleg4@gmail.com>
2022-03-04 18:11:28 +0200

    api fetch

---
+++ pages/test.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- you'll need to handle a loading state -->
+  <div v-if="pending">
+    Loading ...
+  </div>
+  <div v-else>
+    <div v-for="item in characters" :key="item.name">
+      {{ item.name }} <br>
+      {{ item.rarity }} <StarIcon class="h-5 w-5 inline"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// client side CORS error (JOSTY?)
+const { pending, data: characters } = useLazyFetch('https://api-genshin.herokuapp.com/api/characters');
+
+useMeta({
+    title: "api fetch",
+    meta: [
+        { name: 'description', content: 'api page' }
+    ]
+})
+</script>
+
+
+<script>
+import { StarIcon } from '@heroicons/vue/solid'
+
+export default {
+  components: {
+    StarIcon
+  }
+}
+</script>
