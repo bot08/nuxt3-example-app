622237a18d1ac5c8f2f6510b7b6e134516930385
Oleg <scherbaoleg4@gmail.com>
2022-05-13 14:45:04 +0300

    add meta & link example

--- app.vue
+++ app.vue
@@ -12,6 +12,12 @@ useMeta({
   },
   bodyAttrs: {
     class: 'min-h-screen bg-gray-100 dark:bg-gray-800 transition-colors'
-  }
+  },
+  // meta: [
+  //   { name: 'theme-color', content: '#fff' }
+  // ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+  ]
 })
 </script>
